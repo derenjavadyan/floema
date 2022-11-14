9f940aa8d8ebb1433f79491e6811c30f9c84a5ec
derenjavadyan <98952880+derenjavadyan@users.noreply.github.com>
2022-11-14 14:51:24 +0400

    Structuring the views of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CollectionsComponent } from './pages/collections/collections.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavigationComponent } from './partials/navigation/navigation.component';
+import { PreloaderComponent } from './partials/preloader/preloader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    CollectionsComponent,
+    DetailsComponent,
+    HomeComponent,
+    NavigationComponent,
+    PreloaderComponent
   ],
   imports: [
     BrowserModule,
