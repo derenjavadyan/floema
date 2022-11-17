8f87908548c5366f54f4d6125acfba252e80fdd8
derenjavadyan <98952880+derenjavadyan@users.noreply.github.com>
2022-11-17 17:55:46 +0400

    added math.js

--- src/app/pages/collection/collection.component.ts
+++ src/app/pages/collection/collection.component.ts
@@ -1,58 +1,77 @@
 import { Component } from '@angular/core';
 
 interface Collection {
- label: string;
- title: string;
+  label: string;
+  title: string;
 }
 
-interface Gallery{
-  link: string
-  img: string
-  article: galleryItems[]
+interface Gallery {
+  link: string;
+  img: string;
+  article: galleryItems[];
 }
 
-interface galleryItems{
-  title: string
-  desc: string
+interface galleryItems {
+  title: string;
+  desc: string;
+}
+
+//
+
+interface Content {
+  title: string;
+  desc: string;
 }
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
-  styleUrls: ['./collection.component.scss']
+  styleUrls: ['./collection.component.scss'],
 })
 export class CollectionComponent {
-
-  public collectionTitles?:Collection[] = [
+  public collectionTitles: Collection[] = [
     {
-      label: "collection one",
-      title: "Vita"
+      label: 'collection one',
+      title: 'Vita',
     },
     {
-      label: "collection two",
-      title: "Aria"
+      label: 'collection two',
+      title: 'Aria',
     },
     {
-      label: "collection three",
-      title: "Ondole"
+      label: 'collection three',
+      title: 'Ondole',
     },
     {
-      label: "collection four",
-      title: "Treo"
+      label: 'collection four',
+      title: 'Treo',
     },
-  ]
+  ];
 
-  public collectionGallery?:Gallery[]
-
-
-
-
-
-
-
-
-
-
-
-
+  public collectionGallery: Gallery[] = [
+    {
+      link: 'asd',
+      img: '../../../assets/img/gallery/photo.png',
+      article: [
+        { title: 'asd', desc: 'asd' },
+        { title: 'asd', desc: 'asd' },
+      ],
+    },
+    {
+      link: 'asd',
+      img: '../../../assets/img/gallery/photo.png',
+      article: [
+        { title: 'asd', desc: 'asd' },
+        { title: 'asd', desc: 'asd' },
+      ],
+    },
+    {
+      link: 'asd',
+      img: '../../../assets/img/gallery/photo.png',
+      article: [
+        { title: 'asd', desc: 'asd' },
+        { title: 'asd', desc: 'asd' },
+      ],
+    },
+  ];
 }
